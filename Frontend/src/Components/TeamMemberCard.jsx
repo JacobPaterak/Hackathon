1092b4723309bb775b8ec69999cd3dd16475f756
Jacob Paterak <jacobpaterak@gmail.com>
2026-01-24 22:37:27 -0500

    Landing Page Done

--- Frontend/src/Components/TeamMemberCard.jsx
+++ Frontend/src/Components/TeamMemberCard.jsx
@@ -1,8 +1,18 @@
-export default function TeamMemberCard({ name, role }) {
+export default function TeamMemberCard({ name, role, description, photoUrl }) {
   return (
-    <div className="card">
-      <strong>{name}</strong>
-      <div>{role}</div>
+    <div className="team-card">
+      <div className="team-photo">
+        {photoUrl ? <img src={photoUrl} alt={name} /> : null}
+      </div>
+
+      <div className="team-info">
+        <div className="team-header">
+          <div className="team-name">{name}</div>
+          <div className="team-role">{role}</div>
+        </div>
+
+        <div className="team-desc">{description}</div>
+      </div>
     </div>
   );
 }
--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [password, setPassword] = useState("");
